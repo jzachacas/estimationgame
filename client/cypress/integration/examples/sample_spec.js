a3a3f43fcaeb9364adc4146e7dd86fb1e42bcc27
Jan Zacharias <jan.zacharias@cassini.de>
2021-02-08 21:23:45 +0100

    [POKER-66] Slightly improved story page
    and added a new test

--- client/cypress/integration/examples/sample_spec.js
+++ client/cypress/integration/examples/sample_spec.js
@@ -9,13 +9,28 @@ describe('Test login', () => {
   })
 })
 
-describe('Test login', () => {
-  it('logout + login should never fail', () => {
+describe('Test login 2', () => {
+  it('Repeat for a second user', () => {
 
-    cy.request('/logout')
-    cy.visit('/')
-
+    cy.request(`/logout`)
+    cy.visit(`/`)
     cy.get('input').type('I am a second bot')
     cy.get('button').click()
+  })
+})
+
+describe('Change current Story', () => {
+  it('Editing the description', () => {
+    cy.request(`/logout`)
+    cy.request(`/logout`)
+    cy.visit(`/`)
+    cy.get('input').type('I am a second bot')
+    cy.get('button').click()
+
+    cy.get('#edit-story-btn').click()
+    cy.get('#form-title-edit-input').type('{selectall}Me demand new feature')
+    cy.get('#edit-submit').click()
+
+    cy.get('h5.card-title').should('have.text', 'Me demand new feature')
   })
 })
--- client/src/components/Story.vue
+++ client/src/components/Story.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="container">
     <h2>Story</h2>
-    Title: {{ story.title }}<br/>
-    Description: {{ story.description }}<br/>
-    <button
-      type="button"
-      class="btn btn-sm btn-light"
-      v-b-modal.story-update-modal
-      @click="editStory(story)">
-      Update
-    </button>
+
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">{{ story.title }}</h5>
+        <p class="card-text">{{ story.description }}</p>
+        <button
+          type="button"
+          id="edit-story-btn"
+          class="btn btn-sm btn-light"
+          v-b-modal.story-update-modal
+          @click="editStory(story)">
+          Edit story
+        </button>
+      </div>
+    </div>
 
     <b-modal ref="editStoryModal"
              id="story-update-modal"
              title="Update Story"
              hide-footer>
-      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+      <b-form @submit="onSubmitUpdate" @reset="onCancel" class="w-100">
         <b-form-group id="form-title-edit-group"
                       label="Title:"
                       label-for="form-title-edit-input">
@@ -37,8 +43,8 @@
           </b-form-input>
         </b-form-group>
         <b-button-group>
-          <b-button type="submit" variant="primary">Update</b-button>
-          <b-button type="reset" variant="danger">Cancel</b-button>
+          <b-button type="submit" variant="primary" id="edit-submit">Update</b-button>
+          <b-button type="cancel" variant="danger" @click.prevent="onCancel" >Cancel</b-button>
         </b-button-group>
       </b-form>
     </b-modal>
@@ -93,7 +99,8 @@ export default {
       };
       this.updateStory(payload, this.editForm.id);
     },
-    onResetUpdate() {
+    onCancel() {
+      this.$refs.editStoryModal.hide();
     },
     getStory() {
       const path = `${API_LOCATION}/story`;
