88d7a4c8800bfff5aac64919edd45d2e049384c5
Jan Zacharias <jan.zacharias@cassini.de>
2021-03-07 09:46:27 +0100

    Made application runnable as a 'dogu'
    Needed to introduce url prefix in order to support
    proper routing by nginx in Cloudogu Ecosystem (CES).

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,10 @@
-export const API_LOCATION = process.env.NODE_ENV === 'production' ? 'http://localhost/api' : 'http://localhost:8080/api';
-export const WS_LOCATION = process.env.NODE_ENV === 'production' ? 'http://localhost/' : 'http://localhost:8080/';
-export const WS_PATH = '/api-ws/socket.io';
+// backend calls must be prefixed with the same path as frontend calls in production
+// to make nginx forwarding rules in cloudogu/CES work.
+export const API_LOCATION = process.env.NODE_ENV === 'production' ? '/estimo/api' : '/api';
+
+// Websocket location and websocket path
+export const WS_LOCATION = '/';
+export const WS_PATH = '/estimo/api-ws/socket.io';
 
 console.info('config:');
 console.info(process.env.NODE_ENV);
